f3bd8bdc0aed5a7da30b7c0bc432cbe9056d0d6f
Shivam <shivamchaudhary310899@.com>
2025-01-10 14:28:57 +0530

    layout setup

--- simpleform/src/App.js
+++ simpleform/src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [contact, setContact] = useState("");
+    const [subjects, setSubjects] = useState({
+          english: true,
+          maths: false,
+          physics: false,
+    });
+
+
+    const handleSubjectChange = (sub) => {
+      setSubjects((prev) => ({
+        ...prev, 
+        [sub]: !prev[sub],
+      }));
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>This is a React Form</h1>
+      <fieldset>
+        <form action="#" method='get'>
+          <label for="firstname">
+            First Name*
+          </label>
+          <input
+            type="text"
+            name='firstname'
+            id='firstname'
+            value={firstname}
+            onChange={(e) =>
+                setFirstname(e.target.value)
+            } 
+            placeholder='enter first name'
+            required
+          />
+          <label for="lastname">
+            Last Name
+          </label>
+          <input
+              type='text'
+              name='lastname'
+              id='lastname'
+              value={lastname}
+              onChange={(e) => 
+                  setLastName(e.target.value)
+              }
+              placeholder='enter last name' 
+              required
+          />
+          <label for="email">Enter Email
+          </label>
+          <input
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              onChange={(e) => 
+                setEmail(e.target.value)
+              } 
+              placeholder='Enter email'
+              required
+          />
+          <label for="tel">Contact</label>
+          <input
+            type='tel'
+            name='contact'
+            id='contact'
+            value={contact}
+            onChange={(e) => 
+              setContact(e.target.value)
+            }
+            placeholder='enter mobile number'
+            required 
+          />
+          <label for="lang">
+            Your best Subject
+          </label>
+          <input
+              type='checkbox'
+              name='lang'
+              id='english'
+              checked={subjects.english ===
+                true}
+              onChange={(e) => 
+                handleSubjectChange("english")
+              }
+          />
+          English
+          <input
+              type='checkbox'
+              name='lang'
+              id='physics'
+              checked={subjects.physics === true} 
+              onChange={(e) => 
+                handleSubjectChange("physics")
+              }
+          />
+          Physics
+        </form>
+      </fieldset>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
